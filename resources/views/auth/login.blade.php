b2ad95c426c0d9275d3679745e0a9d02d5c21a00
Centertek <centertek@gmail.com>
2018-08-21 23:26:32 -0300

    v4

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,20 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
     <div class="row">
         <div class="col-md-4 col-md-offset-4">
-            .panel
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                <h1 class="panel-title">Acceso a la aplicacion</h1>
+                </div>
+                <div class="panel-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
 
     </div>
